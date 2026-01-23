0f8506fe7a74f04a2afda8a2f3a3f492330e8b08
ArduDev <243449776+ArduDevGit@users.noreply.github.com>
2026-01-22 18:39:04 -0800

    Update config to match training file topology.

--- libraries/AP_NeuralNet/AP_Neural_Config.h
+++ libraries/AP_NeuralNet/AP_Neural_Config.h
@@ -5,15 +5,15 @@
 
 static constexpr unsigned NUM_LAYERS = 3;
 
-static constexpr unsigned NUM_INPUTS = 3;
+static constexpr unsigned NUM_INPUTS = 2;
 
 // These need to stay in sync with NUM_LAYERS.
 // Plus 1 for a bias at every layer.
 static constexpr unsigned NUM_NEURONS_INPUT_LAYER = NUM_INPUTS + 1;
-static constexpr unsigned NUM_NEURONS_LAYER_ONE =   2 + 1;
+static constexpr unsigned NUM_NEURONS_LAYER_ONE =   1 + 1;
 // can add more layers here
 //
-static constexpr unsigned NUM_NEURONS_OUTPUT_LAYER = 1 + 1;
+static constexpr unsigned NUM_NEURONS_OUTPUT_LAYER = 1; //no bias in output layer
 
 // Max number of neurons for each layer (up to 9 with 1 bias)
 static constexpr unsigned MAX_NEURONS = 10;
