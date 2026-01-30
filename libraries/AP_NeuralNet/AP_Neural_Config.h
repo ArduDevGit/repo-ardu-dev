d0e83cfcd7b8ad9b9fec024f7c1020e675a0a045
ArduDev <243449776+ArduDevGit@users.noreply.github.com>
2026-01-29 16:56:36 -0800

    The topology needs to match between the topology listed in trainigData.txt and here.

--- libraries/AP_NeuralNet/AP_Neural_Config.h
+++ libraries/AP_NeuralNet/AP_Neural_Config.h
@@ -7,10 +7,10 @@ static constexpr unsigned NUM_LAYERS = 3;
 
 static constexpr unsigned NUM_INPUTS = 2;
 
-// These need to stay in sync with NUM_LAYERS.
+// These need to stay in sync with NUM_LAYERS and the training data file topology i.e. "topology: 3 3 1"
 // Plus 1 for a bias at every layer.
 static constexpr unsigned NUM_NEURONS_INPUT_LAYER = NUM_INPUTS + 1;
-static constexpr unsigned NUM_NEURONS_LAYER_ONE =   1 + 1;
+static constexpr unsigned NUM_NEURONS_LAYER_ONE =   2 + 1;
 // can add more layers here
 //
 static constexpr unsigned NUM_NEURONS_OUTPUT_LAYER = 1; //no bias in output layer
